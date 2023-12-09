db1bff18abda4bbcd4d6da4bf24f934ba9b6ad4c
Roderick Shoolbraid <rdxenv@gmail.com>
2023-12-08 22:10:50 -0800

    updated database functions:

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -16,37 +16,49 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+const getUserWithEmail = function(email) {
+  const queryString = `SELECT * FROM users WHERE email = $1`;
+  const values = [email.toLowerCase()]; // Ensure email comparison is case-insensitive
+  
+  return pool.query(queryString, values)
+    .then(res => res.rows[0] || null)
+    .catch(err => console.error('query error', err.stack));
 };
+
 
 /**
  * Get a single user from the database given their id.
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+const getUserWithId = function(id) {
+  const queryString = `SELECT * FROM users WHERE id = $1`;
+  const values = [id];
+  
+  return pool.query(queryString, values)
+    .then(res => res.rows[0] || null)
+    .catch(err => console.error('query error', err.stack));
 };
+
 
 /**
  * Add a new user to the database.
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+const addUser = function(user) {
+  const queryString = `
+    INSERT INTO users (name, email, password) 
+    VALUES ($1, $2, $3)
+    RETURNING *;
+  `;
+  const values = [user.name, user.email, user.password];
+  
+  return pool.query(queryString, values)
+    .then(res => res.rows[0])
+    .catch(err => console.error('query error', err.stack));
 };
+
 
 /// Reservations
 
